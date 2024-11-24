a3d4cad258a3972913a954e6443f465aaac498d4
MeetBharodiya <meetbharodiya2002@gmail.com>
2024-11-24 12:14:41 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import app from './app'
 import { Config } from './config'
+import logger from './config/logger'
 
 const startSerevr = () => {
   const PORT = Config.PORT
 
   try {
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    app.listen(PORT, () => {
+      logger.info('Server listening on port', { port: PORT })
+    })
   } catch (err) {
     console.error(err)
     process.exit(1)
