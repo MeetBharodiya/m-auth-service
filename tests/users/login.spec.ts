09b9ed81dbaecd27d7b957409d6b0200c4186326
MeetBharodiya <meetbharodiya2002@gmail.com>
2024-12-23 22:42:37 +0530

    add test case for login

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm'
 import { AppDataSource } from '../../src/config/data-source'
 import app from '../../src/app'
 import { App } from 'supertest/types'
+import { isJwt } from '../utils'
 
 describe('POST /auth/login', () => {
   let connection: DataSource
@@ -26,22 +27,26 @@ describe('POST /auth/login', () => {
   })
 
   describe('Given all fields', () => {
-    // it('should return 200 status code', async () => {
-    //   // Arrange
-    //   const userData = {
-    //     email: 'temp@gmail.com',
-    //     password: 'password123',
-    //   }
+    it('should return 200 status code', async () => {
+      // Arrange
+      const userData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: '9B9yZ@example.com',
+        password: 'password123',
+      }
 
-    //   // Act
-    //   const response = await request(app as unknown as App)
-    //     .post('/auth/login')
-    //     .send(userData)
-    //   console.log(response.body)
+      // Act
+      await request(app as unknown as App)
+        .post('/auth/register')
+        .send(userData)
+      const response = await request(app as unknown as App)
+        .post('/auth/login')
+        .send(userData)
 
-    //   // Assert
-    //   expect(response.statusCode).toBe(200)
-    // })
+      // Assert
+      expect(response.statusCode).toBe(200)
+    })
 
     it('should return valid json response', async () => {
       // Arrange
@@ -61,7 +66,7 @@ describe('POST /auth/login', () => {
       ).toEqual(expect.stringContaining('json'))
     })
 
-    it('should return 400 status code if email does not exist in database', async () => {
+    it('should return 400 status code if email or password is wrong', async () => {
       // Arrange
       const userData = {
         email: '9B9yZ@example.com',
@@ -77,20 +82,46 @@ describe('POST /auth/login', () => {
       expect(response.statusCode).toBe(400)
     })
 
-    it('should return 400 status code if password does not exist in database', async () => {
+    it('should return refresh token and access token in cookie', async () => {
       // Arrange
       const userData = {
+        firstName: 'John',
+        lastName: 'Doe',
         email: '9B9yZ@example.com',
-        password: 'password',
+        password: 'password123',
       }
 
       // Act
+      await request(app as unknown as App)
+        .post('/auth/register')
+        .send(userData)
       const response = await request(app as unknown as App)
         .post('/auth/login')
         .send(userData)
 
       // Assert
-      expect(response.statusCode).toBe(400)
+      interface Header {
+        ['set-cookie']: string[]
+      }
+
+      //Assert
+      let accessToken = null
+      let refreshToken = null
+      const cookies =
+        (response.headers as unknown as Header)['set-cookie'] || []
+
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith('accessToken=')) {
+          accessToken = cookie.split(';')[0].split('=')[1]
+        }
+        if (cookie.startsWith('refreshToken=')) {
+          refreshToken = cookie.split(';')[0].split('=')[1]
+        }
+      })
+      expect(accessToken).not.toBeNull()
+      expect(refreshToken).not.toBeNull()
+      expect(isJwt(accessToken)).toBeTruthy()
+      expect(isJwt(refreshToken)).toBeTruthy()
     })
   })
   describe('Fields are missing', () => {
