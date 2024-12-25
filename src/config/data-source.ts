1f9d09c3d0afc29baf83dbf5f5ac69b56345fa3f
MeetBharodiya <meetbharodiya2002@gmail.com>
2024-12-25 16:06:34 +0530

    add initial migration for exsiting tables

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,9 +12,9 @@ export const AppDataSource = new DataSource({
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
   // value must false in prod environment because data loss can occure
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User, RefreshToken],
-  migrations: [],
+  migrations: ['src/migration/*.ts'],
   subscribers: [],
 })
