9238a6ffcb083d74a0d179891bbf5cc2f3b333bf
MeetBharodiya <meetbharodiya2002@gmail.com>
2024-12-25 15:16:35 +0530

    add logout endpoint

---
+++ src/middlewares/parseRefreshToken.ts
@@ -0,0 +1,14 @@
+// Middleware to return refresh token without validating it
+import { Request } from 'express'
+import { expressjwt } from 'express-jwt'
+import { Config } from '../config'
+import { AuthCookie } from '../types'
+
+export default expressjwt({
+  secret: Config.REFRESH_TOKEN_SECRET!,
+  algorithms: ['HS256'],
+  getToken(req: Request) {
+    const { refreshToken } = req.cookies as AuthCookie
+    return refreshToken
+  },
+})
