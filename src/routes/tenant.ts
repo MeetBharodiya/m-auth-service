5416805af8e79a209d32907b379c211399ae1cb2
MeetBharodiya <meetbharodiya2002@gmail.com>
2024-12-30 20:11:20 +0530

    add test to check logged in user can only create tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -4,14 +4,18 @@ import { TenantService } from '../services/TenantService'
 import { AppDataSource } from '../config/data-source'
 import { Tenant } from '../entity/Tenant'
 import logger from '../config/logger'
+import authenticate from '../middlewares/authenticate'
 
 const router = express.Router()
 const tenantRepository = AppDataSource.getRepository(Tenant)
 const tenantServie = new TenantService(logger, tenantRepository)
 const tenantController = new TenantController(logger, tenantServie)
 
-router.post('/', (req: Request, res: Response, next: NextFunction) =>
-  tenantController.create(req, res, next),
+router.post(
+  '/',
+  authenticate,
+  (req: Request, res: Response, next: NextFunction) =>
+    tenantController.create(req, res, next),
 )
 
 export default router
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -4,19 +4,26 @@ import { AppDataSource } from '../../src/config/data-source'
 import app from '../../src/app'
 import { App } from 'supertest/types'
 import { Tenant } from '../../src/entity/Tenant'
+import createJWKSMock from 'mock-jwks'
+import { Roles } from '../../src/constants'
 
 describe('POST /tenants', () => {
   let connection: DataSource
+  let jwks: ReturnType<typeof createJWKSMock>
+  let adminToken: string
 
   // Setting up tear down for Database like make connection with it, clear before each test case and destroy the connection after all.
 
   // will run before all testcases to make connection with database
   beforeAll(async () => {
+    jwks = createJWKSMock('http://localhost:8000')
     connection = await AppDataSource.initialize()
   })
 
   // will tun before each test case to drop database and synchronize the database for any new added column or anything in schema
   beforeEach(async () => {
+    jwks.start()
+    adminToken = jwks.token({ sub: '1', role: Roles.ADMIN })
     await connection.dropDatabase()
     await connection.synchronize()
   })
@@ -24,6 +31,12 @@ describe('POST /tenants', () => {
   // will run after all testcases run to close the connection with Database
   afterAll(async () => {
     await connection.destroy()
+  })
+
+  // After each test case stop the mock server
+  afterEach(() => {
+    // stop the mock server after each test
+    jwks.stop()
   })
 
   describe('Given all fields', () => {
@@ -37,6 +50,7 @@ describe('POST /tenants', () => {
       // Act
       const response = await request(app as unknown as App)
         .post('/tenants')
+        .set('Cookie', [`accessToken=${adminToken};`])
         .send(tenantData)
 
       // Assert
@@ -53,6 +67,7 @@ describe('POST /tenants', () => {
       // Act
       await request(app as unknown as App)
         .post('/tenants')
+        .set('Cookie', [`accessToken=${adminToken};`])
         .send(tenantData)
 
       // Assert
@@ -61,6 +76,25 @@ describe('POST /tenants', () => {
       expect(tenants).toHaveLength(1)
       expect(tenants[0].name).toBe(tenantData.name)
       expect(tenants[0].address).toBe(tenantData.address)
+    })
+
+    it('should return 401 status code if user is not authenticated ', async () => {
+      // Arrange
+      const tenantData = {
+        name: 'Tenant1',
+        address: 'Address1',
+      }
+
+      // Act
+      const response = await request(app as unknown as App)
+        .post('/tenants')
+        .send(tenantData)
+
+      // Assert
+      const tenantRepository = connection.getRepository(Tenant)
+      const tenants = await tenantRepository.find()
+      expect(response.statusCode).toBe(401)
+      expect(tenants).toHaveLength(0)
     })
   })
   describe('Fields are missing', () => {})
